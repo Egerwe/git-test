a1bf381c35cb607d604c47a86f405c5aca53e672
Chris <Chris@qq.com>
2022-09-26 15:34:28 +0800

    first commit

---
+++ src/main/java/com/Yao/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.Yao.git;
+
+/**
+ * Author Yjw
+ * 2022/9/26 15:26
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
